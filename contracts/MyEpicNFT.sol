db4c2b2aa2802a575dbf9999a1a9cda3c1e1e80c
Himanshu Purohit <himanshu1203@gmail.com>
2022-02-19 22:49:30 +0530

    NewEpicNFTMinted event added

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -13,6 +13,8 @@ contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  event NewEpicNFTMinted(address sender, uint256 tokenId); 
+
   // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
   // So, we make a baseSvg variable here that all our NFTs can use.
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
@@ -70,8 +72,10 @@ contract MyEpicNFT is ERC721URIStorage {
   
     // We'll be setting the tokenURI later!
     _setTokenURI(newItemId, "blah");
-  
+
     _tokenIds.increment();
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    
+    emit NewEpicNFTMinted(msg.sender, newItemId);
   }
 } 
